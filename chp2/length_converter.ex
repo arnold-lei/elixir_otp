a68ba8bc280b11c8a98404cc3219e60e873ad0f7
Arnold Lei <arnold.lei88@gmail.com>
2016-12-03 01:43:07 -0500

    trying different syntax

--- chp2/length_converter.ex
+++ chp2/length_converter.ex
@@ -1,7 +1,17 @@
-    defmodule MeterToFootConverter do
-        def convert(m) do
-            m * 3.28084
-        end
-    end
+    # defmodule MeterToLengthConverter do
+    #     defmodule Feet do
+    #         def convert(m) do
+    #             m * 3.28084
+    #         end
+    #     end
+    #     defmodule Inches do
+    #         def convert(m) do
+    #             m * 39.3701
+    #         end
+    #     end
+    # end
 
-    
+    defmodule MeterToLengthConverter do
+        def convert(:feet, m), do: m * 3.28084
+        def convert(:inch, m), do: m * 39.3701
+    end
